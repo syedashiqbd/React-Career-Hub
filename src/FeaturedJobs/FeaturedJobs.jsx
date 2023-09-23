eac37d0e8c7f6c39f0dd34f8dc321216329731ed
Syed Ashiq <imsyedashiq@gmail.com>
2023-09-23 18:55:40 +0600

    featured jobs card added and show all button added

--- src/FeaturedJobs/FeaturedJobs.jsx
+++ src/FeaturedJobs/FeaturedJobs.jsx
@@ -1,23 +1,34 @@
-import { data } from 'autoprefixer';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import JobsCard from '../JobsCard/JobsCard';
 
 const FeaturedJobs = () => {
   const [jobs, setJobs] = useState();
+  const [dataLength, setDataLength] = useState(2);
 
   useEffect(() => {
     fetch('jobs.json')
       .then((res) => res.json())
       .then((data) => setJobs(data));
   }, []);
-  console.log(jobs);
   return (
     <div className="w-10/12 md:w-9/12 mx-auto my-8">
-      <div>
-        {jobs?.map((job) => (
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+        {jobs?.slice(0, dataLength).map((job) => (
           <JobsCard key={job.id} job={job}></JobsCard>
         ))}
+      </div>
+      <div
+        className={
+          dataLength === jobs?.length ? 'hidden' : 'flex justify-center'
+        }
+      >
+        <button
+          onClick={() => setDataLength(jobs?.length)}
+          className="btn btn-primary mt-8"
+        >
+          Show All Jobs
+        </button>
       </div>
     </div>
   );
--- src/JobsCard/JobsCard.jsx
+++ src/JobsCard/JobsCard.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/prop-types */
+import { MdLocationOn } from 'react-icons/md';
+import { AiFillDollarCircle, AiOutlineDollarCircle } from 'react-icons/ai';
+
 const JobsCard = ({ job }) => {
   const {
     logo,
@@ -6,13 +10,40 @@ const JobsCard = ({ job }) => {
     job_type,
     remote_or_onsite,
     location,
-    salalry,
+    salary,
   } = job;
   return (
-    <div>
-      <img src={logo} />
-      <p>{job_title}</p>
-      <p>{company_name}</p>
+    <div className="rounded-lg  bg-base-100 border">
+      <figure className="mt-6 ml-8">
+        <img src={logo} alt="" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{job_title}!</h2>
+
+        <p>{company_name}</p>
+        <div className="my-4">
+          <button className="py-2 px-5 border border-[#7E90FE] rounded text-[#7E90FE] mr-4 font-bold">
+            {remote_or_onsite}
+          </button>
+          <button className="py-2 px-5 border border-[#7E90FE] rounded text-[#7E90FE] mr-4 font-bold">
+            {job_type}
+          </button>
+        </div>
+        <div className="mb-4 flex gap-6">
+          <h2 className="flex items-center gap-2">
+            <MdLocationOn className="text-xl"></MdLocationOn>
+            {location}
+          </h2>
+          <h2 className="flex items-center gap-2">
+            <AiOutlineDollarCircle className="text-xl"></AiOutlineDollarCircle>
+            {salary}
+          </h2>
+        </div>
+
+        <div className="card-actions ">
+          <button className="btn btn-primary">View Details</button>
+        </div>
+      </div>
     </div>
   );
 };
